a665df39049e0cde81fba3a078b9fdafe91039d3
Kamil Paprocki <kamil.paprocki1@gmail.com>
2023-05-24 16:45:21 +0200

    init commit

---
+++ src/main/java/com/boardgametracker/BoardGameTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.boardgametracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardGameTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardGameTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boardgametracker/BoardGameTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boardgametracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardGameTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
